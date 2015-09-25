bb424eb2d1ffa69c97887c42e8e13dd678072dab
Sean McGeer <basbalmanic@gmail.com>
2015-09-24 19:20:16 -0700

    Initial systems work.

---
+++ src/System.h
@@ -0,0 +1,16 @@
+/*!
+    \file   System.h
+    \author Sean McGeer
+    \date   9/18/15
+    \brief
+        Defines the System class, which all systems should inherit from.
+    \see System.cpp
+*/
+
+namespace ForLeaseEngine {
+
+    class System }
+
+    };
+
+}
