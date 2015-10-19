08968582fa3cef4757c18093da998734dca95e3a
hudsonchris <c.hudson@digipen.edu>
2015-10-18 23:07:23 -0700

    Added mouse button event, added input handling for mouse

---
+++ src/Mouse.h
@@ -0,0 +1,21 @@
+#ifndef MOUSE_H
+#define MOUSE_H
+
+#include <SDL.H>
+
+namespace ForLeaseEngine {
+    class MouseButtonState {
+        static const int Pressed = SDL_PRESSED;
+        static const int Released = SDL_RELEASED;
+    };
+
+    class MouseButton {
+        static const int Left = SDL_BUTTON_LEFT;
+        static const int Middle = SDL_BUTTON_MIDDLE;
+        static const int Right = SDL_BUTTON_RIGHT;
+        static const int ButtonFour = SDL_BUTTON_X1;
+        static const int ButtonFive = SDL_BUTTON_X2;
+    };
+}
+
+#endif // MOUSE_H
