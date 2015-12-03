1bbfccc559232d1a0a3516559a4d80e8df8209cf
Sean McGeer <basbalmanic@gmail.com>
2015-12-03 02:03:44 -0800

    Fixed collision issues--mroe to come.

--- testing/sean/main.cpp
+++ testing/sean/main.cpp
@@ -22,12 +22,13 @@ int Start(){
     std::vector<ForLeaseEngine::State *> states;
     //ForLeaseEngine::SegmentPrototypeState* state = new ForLeaseEngine::SegmentPrototypeState();
     //SeanState* state = new SeanState();
-    //states.push_back(state);
+    states.push_back(new SeanState());
     //states.push_back(new SecondState());
+    states = LoadLevels("Game.json", states);
     //states = LoadLevels("Game.json", states);
     //states.push_back(new RaycastTest());
 
-    states = LoadLevels("Game.json", states);
+    //states = LoadLevels("Game.json", states);
 
     ForLeaseEngine::Engine engine(states, 1024, 768, 60);
     engine.Run();
