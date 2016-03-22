28a20fb747234863f91197a4bca9646453ecf98c
Sean McGeer <seanmcgeer@gmail.com>
2016-03-21 17:32:38 -0700

    Save-work commit that tells me that moving platforms need work.

--- testing/sean/SeanState.cpp
+++ testing/sean/SeanState.cpp
@@ -52,9 +52,16 @@ void SeanState::Load() {
     Entity* floor = AddEntity("Floor");
     floor->AddComponent(new Components::Transform(*floor, Point(0,-4), 20, 1, 0, 0));
     floor->AddComponent(new Components::Physics(*floor, 1.0f, Vector(0, 0), Vector(0, 0), Vector(0, 0), false, true));
-    floor->AddComponent(new Components::EnemyPace(*floor, 3, 20, 0));
+    floor->AddComponent(new Components::EnemyPace(*floor, 3, 20, 0, 1));
     floor->AddComponent(new Components::Collision(*floor, 2, 2, true, 0, 0, true));
     floor->AddComponent(new Components::Model(*floor, true, false, false, "1-1Block.json"));
+
+    Entity* floor2 = AddEntity("Floor2");
+    floor2->AddComponent(new Components::Transform(*floor2, Point(0, -6), 30, 1, 0, 0));
+    //floor2->AddComponent(new Components::Physics(*floor2, 1.0f, Vector(0, 0), Vector(0, 0), Vector(0, 0), false, true));
+    //floor2->AddComponent(new Components::EnemyPace(*floor2, 3, 20, 0));
+    floor2->AddComponent(new Components::Collision(*floor2, 2, 2, true, 0, 0, true));
+    floor2->AddComponent(new Components::Model(*floor, true, false, false, "1-1Block.json"));
 
     Entity* ceil = AddEntity("Ceiling");
     ceil->AddComponent(new Components::Transform(*ceil, Point(0, 15), 20, 1, 0, 0));
